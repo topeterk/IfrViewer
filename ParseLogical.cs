135301c5e82fd5ad77ae428c2ddb92ac5da1c3fc
Peter Kirmeier <topeterk@freenet.de>
2017-02-04 22:19:26 +0100

    add string translation to logical view of FormSetsrequires loading string packages BEFORE the corresponding form packages

--- ParseLogical.cs
+++ ParseLogical.cs
@@ -44,10 +44,16 @@ namespace IfrViewer
             }
         };
 
+        private struct StringDataBase
+        {
+            public string Language;
+            public List<KeyValuePair<UInt16, string>> Strings;
+        }
 
         public static List<ParsedHpkNode> ParseHpkPackages(List<HiiPackageBase> Packages)
         {
             List<ParsedHpkNode> result = new List<ParsedHpkNode>();
+            List<StringDataBase> StringDB = new List<StringDataBase>();
 
             foreach (HiiPackageBase pkg in Packages)
             {
@@ -61,17 +67,22 @@ namespace IfrViewer
                             root.Name = "Form Package";
                             result.Add(root);
                             foreach (HPKElement child in root.Origin.Childs)
-                                ParsePackageIfr(child, root, Packages);
+                                ParsePackageIfr(child, root, Packages, StringDB);
+
+                            // clean up..
+                            StringDB = new List<StringDataBase>();
                             break;
                         case EFI_HII_PACKAGE_e.EFI_HII_PACKAGE_STRINGS:
                             UInt16 StringID = 0;
-                            root.Name = "Strings Package \"" + ((HiiPackageString.Payload_t)pkg.Payload).Language + "\"";
+                            StringDataBase db;
+                            db.Language = ((HiiPackageString.Payload_t)pkg.Payload).Language;
+                            db.Strings = new List<KeyValuePair<UInt16, string>>();
+                            StringDB.Add(db);
+
+                            root.Name = "Strings Package \"" + db.Language + "\"";
                             result.Add(root);
                             foreach (HPKElement child in root.Origin.Childs)
-                            {
-                                StringID++;
-                                ParsePackageSibt(child, root, Packages, ref StringID);
-                            }
+                                ParsePackageSibt(child, root, Packages, ref StringID, db);
                             break;
                         default:
                             CreateLogEntryParser(LogSeverity.UNIMPLEMENTED, root.Name);
@@ -89,7 +100,7 @@ namespace IfrViewer
             return result;
         }
 
-        private static void ParsePackageIfr(HPKElement hpkelem, ParsedHpkNode root, List<HiiPackageBase> Packages)
+        private static void ParsePackageIfr(HPKElement hpkelem, ParsedHpkNode root, List<HiiPackageBase> Packages, List<StringDataBase> StringDB)
         {
             ParsedHpkNode branch = new ParsedHpkNode(hpkelem, hpkelem.Name);
             HiiIfrOpCode elem = (HiiIfrOpCode)hpkelem;
@@ -97,45 +108,80 @@ namespace IfrViewer
             switch (elem.OpCode)
             {
                 case EFI_IFR_OPCODE_e.EFI_IFR_FORM_SET_OP:
+                    EFI_IFR_FORM_SET hdr = (EFI_IFR_FORM_SET)elem.Header;
+                    ParsedHpkNode leaf = new ParsedHpkNode(hpkelem, "Settings");
+                    ParsedHpkNode setting = new ParsedHpkNode(hpkelem, "");
+                    branch.Name = "FormSet \"" + GetStringOfPackages(StringDB, hdr.FormSetTitle) + "\"";
+                    branch.Childs.Add(leaf);
+                    setting.Name = "Help = " + GetStringOfPackages(StringDB, hdr.Help);
+                    leaf.Childs.Add(setting);
+                    setting.Name = "Guid = " + hdr.Guid.Guid.ToString();
+                    leaf.Childs.Add(setting);
+                    foreach (EFI_GUID classguid in (List<EFI_GUID>)elem.Payload)
+                    {
+                        setting.Name = "ClassGuid = " + classguid.Guid.ToString();
+                        leaf.Childs.Add(setting);
+                    }
                     foreach (HiiIfrOpCode child in elem.Childs)
                     {
                         switch (child.OpCode)
                         {
-                            default: ParsePackageIfr(child, branch, Packages); break;
+                            default: ParsePackageIfr(child, branch, Packages, StringDB); break;
                         }
                     }
                     break;
                 case EFI_IFR_OPCODE_e.EFI_IFR_END_OP: return; // Skip
                 default:
                     foreach (HiiIfrOpCode child in elem.Childs)
-                        ParsePackageIfr(child, branch, Packages);
+                        ParsePackageIfr(child, branch, Packages, StringDB);
                     break; // simply add all others 1:1 when no specific handler exists
             }
 
             root.Childs.Add(branch);
         }
 
-        private static void ParsePackageSibt(HPKElement hpkelem, ParsedHpkNode root, List<HiiPackageBase> Packages, ref UInt16 StringID)
+        private static void ParsePackageSibt(HPKElement hpkelem, ParsedHpkNode root, List<HiiPackageBase> Packages, ref UInt16 StringID, StringDataBase db)
         {
             ParsedHpkNode branch = new ParsedHpkNode(hpkelem, hpkelem.Name);
             HiiSibtBlockBase elem = (HiiSibtBlockBase)hpkelem;
 
             switch (elem.BlockType)
             {
-                case EFI_HII_SIBT_e.EFI_HII_SIBT_STRING_UCS2: branch.Name = StringID.ToString("00000") + " " + ((HiiSibtBlockStringUcs2.Payload_t)elem.Payload).StringText; break;
+                case EFI_HII_SIBT_e.EFI_HII_SIBT_STRING_UCS2:
+                    string StringText = ((HiiSibtBlockStringUcs2.Payload_t)elem.Payload).StringText;
+                    StringID++;
+                    branch.Name = StringID.ToString("00000") + " " + StringText;
+                    db.Strings.Add(new KeyValuePair<ushort, string>(StringID, StringText));
+                    break;
                 case EFI_HII_SIBT_e.EFI_HII_SIBT_SKIP1: StringID += ((EFI_HII_SIBT_SKIP1_BLOCK)elem.Header).SkipCount; return; // Skip
                 case EFI_HII_SIBT_e.EFI_HII_SIBT_SKIP2: StringID += ((EFI_HII_SIBT_SKIP2_BLOCK)elem.Header).SkipCount; return; // Skip
                 case EFI_HII_SIBT_e.EFI_HII_SIBT_END: return; // Skip
                 default:
                     foreach (HiiSibtBlockBase child in elem.Childs)
-                    {
-                        StringID++;
-                        ParsePackageSibt(child, branch, Packages, ref StringID);
-                    }
+                        ParsePackageSibt(child, branch, Packages, ref StringID, db);
                     break; // simply add all others 1:1 when no specific handler exists
             }
 
             root.Childs.Add(branch);
+        }
+
+        private static string GetStringOfPackages(List<StringDataBase> StringDB, UInt16 StringID, string Language = "en-US")
+        {
+            foreach (StringDataBase db in StringDB)
+            {
+                if (db.Language != Language) continue;
+
+                foreach (KeyValuePair<UInt16, string> entry in db.Strings)
+                {
+                    if (entry.Key == StringID)
+                    {
+                        return entry.Value;
+                    }
+                }
+            }
+
+            CreateLogEntryParser(LogSeverity.WARNING, "StringID " + StringID + " could not be translated!");
+            return "UNKNOWN_STRING_ID(" + StringID + ")";
         }
 
         private static void CreateLogEntryParser(LogSeverity severity, string msg)
