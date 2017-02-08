a9916c1ce80eeddf548a29c941f2429f880d49fa
Peter Kirmeier <topeterk@freenet.de>
2017-02-08 22:12:35 +0100

    add support for displaying evaluated logic expressions add internal stack implementation of IFR logic supports the following logic opcodes: EFI_IFR_EQ_ID_VAL_OP, EFI_IFR_EQ_ID_ID_OP, EFI_IFR_EQ_ID_VAL_LIST_OP EFI_IFR_QUESTION_REF1_OP, EFI_IFR_QUESTION_REF2_OP, EFI_IFR_QUESTION_REF3_OP EFI_IFR_FALSE_OP, EFI_IFR_TRUE_OP, EFI_IFR_UNDEFINED_OP, EFI_IFR_ZERO_OP EFI_IFR_UINT8_OP, EFI_IFR_UINT16_OP, EFI_IFR_UINT32_OP, EFI_IFR_UINT64_OP EFI_IFR_AND_OP, EFI_IFR_EQUAL_OP, EFI_IFR_GREATER_EQUAL_OP, EFI_IFR_GREATER_THAN_OP, EFI_IFR_LESS_EQUAL_OP, EFI_IFR_LESS_THAN_OP, EFI_IFR_NOT_EQUAL_OP, EFI_IFR_OR_OP, EFI_IFR_NOT_OP EFI_IFR_ADD_OP, EFI_IFR_SUBTRACT_OP EFI_IFR_GET_OP EFI_IFR_STRING_REF1_OP EFI_IFR_END_OP

--- ParseLogical.cs
+++ ParseLogical.cs
@@ -202,7 +202,7 @@ namespace IfrViewer
                                 break;
                             default:
                                 InfoString = "?";
-                                CreateLogEntryParser(LogSeverity.WARNING, "Unknown Ref type [" + hpkelem.UniqueID + "]!");
+                                CreateLogEntryParser(LogSeverity.WARNING, "Unknown reference type [" + hpkelem.UniqueID + "]!");
                                 break;
                         }
                         branch.Name = "Reference to [" + InfoString + "]" + (RefName != null ? " \"" + RefName + "\"" : "");
@@ -268,10 +268,8 @@ namespace IfrViewer
                             CreateLogEntryParser(LogSeverity.WARNING, "Too few logic elements [" + hpkelem.UniqueID + "]!");
                         else
                         {
-                            branch.Name += GetIfrLogicString((HiiIfrOpCode)elem.Childs[0]);
-#warning "As long as Logic String is not complete, keep logic entries in tree"
-for (int i = 0; i < elem.Childs.Count; i++) // skip first element, because it contains the (nested) logic
-                            //for (int i = 1; i < elem.Childs.Count; i++) // skip first element, because it contains the (nested) logic
+                            branch.Name += GetIfrLogicString((HiiIfrOpCode)elem.Childs[0], StringDB);
+                            for (int i = 1; i < elem.Childs.Count; i++) // skip first element, because it contains the (nested) logic
                                 ParsePackageIfr(elem.Childs[i], branch, Packages, StringDB);
                             bProcessChilds = false;
                         }
@@ -306,69 +304,190 @@ for (int i = 0; i < elem.Childs.Count; i++) // skip first element, because it co
             root.Childs.Add(branch);
         }
 
-        private static string GetIfrLogicString(HiiIfrOpCode ifrelem, string NestedResult = "")
+        #region Logic stack
+        private class IfrLogicStack
         {
-            string result = "";
+            public List<string> Elements;
+            public IfrLogicStack() { Elements = new List<string>(); }
+        }
+        private static void Push(this IfrLogicStack Stack, string LogicStr)
+        {
+            Stack.Elements.Add(LogicStr);
+        }
+        private static string Pop(this IfrLogicStack Stack)
+        {
+            if (Stack.Elements.Count == 0)
+            {
+                CreateLogEntryParser(LogSeverity.WARNING, "Logic stack empty!");
+                return "EMPTYSTACK";
+            }
+            string LogicStr = Stack.Elements[Stack.Elements.Count - 1];
+            Stack.Elements.RemoveAt(Stack.Elements.Count - 1);
+            return LogicStr;
+        }
+        private static void Op12(this IfrLogicStack Stack, string Operation)
+        {
+            string expr1 = Stack.Pop();
+            string expr2 = Stack.Pop();
+            Stack.Push("(" + expr1 + " " + Operation + " " + expr2 + ")");
+        }
+        private static void Op21(this IfrLogicStack Stack, string Operation)
+        {
+            string expr1 = Stack.Pop();
+            string expr2 = Stack.Pop();
+            Stack.Push("(" + expr2 + " " + Operation + " " + expr1 + ")");
+        }
+        #endregion
 
+        private static string GetIfrLogicString(HiiIfrOpCode ifrelem, List<StringDataBase> StringDB)
+        {
+            IfrLogicStack Stack = new IfrLogicStack();
+            string LogicString;
+
+            ParseIfrLogicStack(ifrelem, Stack, StringDB);
+
+            if (Stack.Elements.Count != 1)
+            {
+                string StackElements = "";
+                foreach (string element in Stack.Elements) StackElements += Environment.NewLine + "   " + element;
+                CreateLogEntryParser(LogSeverity.WARNING, "Logic stack has " + Stack.Elements.Count + " elements but must have 1 only [" + ifrelem.UniqueID + "]!" + StackElements);
+
+                StackElements = "INVALIDSTACK(";
+                foreach (string element in Stack.Elements) StackElements += element + ",";
+                LogicString = StackElements + ")";
+            }
+            else LogicString = Stack.Elements[0];
+
+            return LogicString;
+        }
+        
+        private static void ParseIfrLogicStack(HiiIfrOpCode ifrelem, IfrLogicStack Stack, List<StringDataBase> StringDB)
+        {
             switch (ifrelem.OpCode)
             {
-                case EFI_IFR_OPCODE_e.EFI_IFR_LOCKED_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_AND_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_OR_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_NOT_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_TO_LOWER_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_TO_UPPER_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_MATCH_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_EQUAL_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_NOT_EQUAL_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_GREATER_THAN_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_GREATER_EQUAL_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_LESS_THAN_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_LESS_EQUAL_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_BITWISE_AND_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_BITWISE_OR_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_BITWISE_NOT_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_SHIFT_LEFT_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_SHIFT_RIGHT_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_ADD_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_SUBTRACT_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_MULTIPLY_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_DIVIDE_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_MODULO_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_QUESTION_REF2_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_TRUE_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_FALSE_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_TO_UINT_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_TO_BOOLEAN_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_MID_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_TOKEN_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_STRING_REF2_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_CONDITIONAL_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_ZERO_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_ONE_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_ONES_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_UNDEFINED_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_LENGTH_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_DUP_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_THIS_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_VALUE_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_CATENATE_OP:
-                case EFI_IFR_OPCODE_e.EFI_IFR_MODAL_TAG_OP: result = NestedResult + " " + ifrelem.OpCode.ToString(); break;
-                case EFI_IFR_OPCODE_e.EFI_IFR_END_OP: return NestedResult;
+                #region Logic OpCodes
+                // -- built-in-function
+                // EFI_IFR_DUP
+                case EFI_IFR_OPCODE_e.EFI_IFR_EQ_ID_VAL_OP:
+                    {
+                        EFI_IFR_EQ_ID_VAL ifr_hdr = (EFI_IFR_EQ_ID_VAL)ifrelem.Header;
+                        Stack.Push("(ValueOfId(" + ifr_hdr.QuestionId.ToString("00000") + ") == " + ifr_hdr.Value.ToString() + ")");
+                    }
+                    break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_EQ_ID_ID_OP:
+                    {
+                        EFI_IFR_EQ_ID_ID ifr_hdr = (EFI_IFR_EQ_ID_ID)ifrelem.Header;
+                        Stack.Push("(ValueOfId(" + ifr_hdr.QuestionId1.ToString("00000") + ") == ValueOfId(" + ifr_hdr.QuestionId2.ToString("00000") + "))");
+                    }
+                    break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_EQ_ID_VAL_LIST_OP:
+                    {
+                        string ValueListStr = "";
+                        foreach (IfrTypeUINT16 value in (List<IfrTypeUINT16>)ifrelem.Payload) ValueListStr += value.u16.ToString() + ",";
+                        Stack.Push("(ValueOfId(" + ((EFI_IFR_EQ_ID_VAL_LIST)ifrelem.Header).QuestionId.ToString("00000") + ") is in [" + ValueListStr + "])");
+                    }
+                    break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_GET_OP:
+                    {
+                        EFI_IFR_GET ifr_hdr = (EFI_IFR_GET)ifrelem.Header;
+                        Stack.Push("ValueFromVar(Id = " + ifr_hdr.VarStoreId.ToString("00000")
+                            + ", Offset/Name = " + ifr_hdr.VarStoreInfo.VarOffset.ToString("00000")
+                            + ", Type = " + ifr_hdr.VarStoreType.ToString() + ")");
+                    }
+                    break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_QUESTION_REF1_OP: Stack.Push("ValueOfId(" + ((EFI_IFR_QUESTION_REF1)ifrelem.Header).QuestionId.ToString("00000") + ")"); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_QUESTION_REF3_OP:
+                    {
+                        switch (ifrelem.Header.GetType().Name)
+                        {
+                            case "EFI_IFR_QUESTION_REF3": Stack.Push("ValueOfId(" + Stack.Pop() + ")"); break;
+                            case "EFI_IFR_QUESTION_REF3_2":
+                                {
+                                    EFI_IFR_QUESTION_REF3_2 ifr_hdr = (EFI_IFR_QUESTION_REF3_2)ifrelem.Header;
+                                    Stack.Push("ValueOfId(" + Stack.Pop() + (ifr_hdr.DevicePath == 0 ? "" : ", DevicePath = \"" + GetStringOfPackages(StringDB, ifr_hdr.DevicePath, ifrelem.UniqueID) + "\"") + ")");
+                                }
+                                break;
+                            case "EFI_IFR_QUESTION_REF3_3":
+                                {
+                                    EFI_IFR_QUESTION_REF3_3 ifr_hdr = (EFI_IFR_QUESTION_REF3_3)ifrelem.Header;
+                                    Stack.Push("ValueOfId(" + Stack.Pop() + ", Guid = " + ifr_hdr.Guid.Guid.ToString() + (ifr_hdr.DevicePath == 0 ? "" : ", DevicePath = \"" + GetStringOfPackages(StringDB, ifr_hdr.DevicePath, ifrelem.UniqueID) + "\"") + ")");
+                                }
+                                break;
+                            default:
+                                Stack.Push("UNKNWONOPCODE(" + ifrelem.OpCode.ToString() + ")");
+                                CreateLogEntryParser(LogSeverity.UNIMPLEMENTED, "Logic reference type " + ifrelem.OpCode.ToString() + " [" + ifrelem.UniqueID + "]!");
+                                break;
+                        }
+                    }
+                    break;
+// EFI_IFR_RULE_REF
+                case EFI_IFR_OPCODE_e.EFI_IFR_STRING_REF1_OP: Stack.Push("\"" + GetStringOfPackages(StringDB, ((EFI_IFR_STRING_REF1)ifrelem.Header).StringId, ifrelem.UniqueID) + "\""); break;
+/* EFI_IFR_THIS |
+EFI_IFR_SECURITY */
+                // -- constant
+                case EFI_IFR_OPCODE_e.EFI_IFR_FALSE_OP: Stack.Push("FALSE"); break;
+/* EFI_IFR_ONE |
+EFI_IFR_ONES */
+                case EFI_IFR_OPCODE_e.EFI_IFR_TRUE_OP: Stack.Push("TRUE"); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_UINT8_OP: Stack.Push(((EFI_IFR_UINT8)ifrelem.Header).Value.ToString()); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_UINT16_OP: Stack.Push(((EFI_IFR_UINT16)ifrelem.Header).Value.ToString()); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_UINT32_OP: Stack.Push(((EFI_IFR_UINT32)ifrelem.Header).Value.ToString()); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_UINT64_OP: Stack.Push(((EFI_IFR_UINT64)ifrelem.Header).Value.ToString()); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_UNDEFINED_OP: Stack.Push("Undefined"); break;
+// EFI_IFR_VERSION
+                case EFI_IFR_OPCODE_e.EFI_IFR_ZERO_OP: Stack.Push("0"); break;
+                // -- binary-op
+                case EFI_IFR_OPCODE_e.EFI_IFR_ADD_OP: Stack.Op21("+"); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_AND_OP: Stack.Op12("&&"); break;
+/* EFI_IFR_BITWISE_AND |
+EFI_IFR_BITWISE_OR |
+EFI_IFR_CATENATE |
+EFI_IFR_DIVIDE */
+                case EFI_IFR_OPCODE_e.EFI_IFR_EQUAL_OP: Stack.Op12("=="); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_GREATER_EQUAL_OP: Stack.Op12(">="); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_GREATER_THAN_OP: Stack.Op12(">"); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_LESS_EQUAL_OP: Stack.Op12("<="); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_LESS_THAN_OP: Stack.Op12("<"); break;
+/*EFI_IFR_MATCH |
+EFI_IFR_MATCH2 |
+EFI_IFR_MODULO |
+EFI_IFR_MULTIPLY */
+                case EFI_IFR_OPCODE_e.EFI_IFR_NOT_EQUAL_OP: Stack.Op12("!="); break;
+                case EFI_IFR_OPCODE_e.EFI_IFR_OR_OP: Stack.Op12("||"); break;
+/* EFI_IFR_SHIFT_LEFT |
+EFI_IFR_SHIFT_RIGHT */
+                case EFI_IFR_OPCODE_e.EFI_IFR_SUBTRACT_OP: Stack.Op21("-"); break;
+                // -- unary-op
+// EFI_IFR_LENGTH
+                case EFI_IFR_OPCODE_e.EFI_IFR_NOT_OP: Stack.Push("!" + Stack.Pop()); break;
+// EFI_IFR_BITWISE_NOT
+                case EFI_IFR_OPCODE_e.EFI_IFR_QUESTION_REF2_OP: Stack.Push("ValueOfId(" + Stack.Pop() + ")"); break;
+/* EFI_IFR_SET |
+EFI_IFR_STRING_REF2 |
+EFI_IFR_TO_BOOLEAN |
+EFI_IFR_TO_STRING |
+EFI_IFR_TO_UINT |
+EFI_IFR_TO_UPPER |
+EFI_IFR_TO_LOWER */
+                // -- ternary-op
+/*EFI_IFR_CONDITIONAL |
+EFI_IFR_FIND |
+EFI_IFR_MID |
+EFI_IFR_TOKEN |
+EFI_IFR_SPAN */
+                #endregion
+                case EFI_IFR_OPCODE_e.EFI_IFR_END_OP: return;
                 default:
-                    //CreateLogEntryParser(LogSeverity.UNIMPLEMENTED, "Logic OpCode " + ifrelem.OpCode.ToString() + " [" + ifrelem.UniqueID + "]!");
-                    result = NestedResult + " " + ifrelem.OpCode.ToString(); break;
+                    Stack.Push("UNKNWONOPCODE(" + ifrelem.OpCode.ToString() + ")");
+                    CreateLogEntryParser(LogSeverity.UNIMPLEMENTED, "Logic OpCode " + ifrelem.OpCode.ToString() + " [" + ifrelem.UniqueID + "]!");
+                    break;
             }
 
             if (ifrelem.HasOwnScope)
             {
-                NestedResult = result;
                 foreach (HiiIfrOpCode child in ifrelem.Childs)
-                    NestedResult = GetIfrLogicString(child, NestedResult);
-                result = "(" + NestedResult + ")";
+                    ParseIfrLogicStack(child, Stack, StringDB);
             }
-
-            return result;
         }
 
         private static void ParsePackageSibt(HPKElement hpkelem, ParsedHpkNode root, List<HiiPackageBase> Packages, ref UInt16 StringID, StringDataBase db)
