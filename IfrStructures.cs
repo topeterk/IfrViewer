ac52f158df727928e354254a92212f42f6a9078c
Peter Kirmeier <topeterk@users.noreply.github.com>
2026-04-02 19:41:37 +0200

    Update to latest UEFI form representation headers based on EDKII commit: 0049ac1793c4abd0652652286e5506f52aaaa0d4 (no effective changes)

--- IfrStructures.cs
+++ IfrStructures.cs
@@ -2424,7 +2424,17 @@ namespace IfrViewer
         EfiKeyF12,
         EfiKeyPrint,
         EfiKeySLck,
-        EfiKeyPause
+        EfiKeyPause,
+        EfiKeyIntl0,
+        EfiKeyIntl1,
+        EfiKeyIntl2,
+        EfiKeyIntl3,
+        EfiKeyIntl4,
+        EfiKeyIntl5,
+        EfiKeyIntl6,
+        EfiKeyIntl7,
+        EfiKeyIntl8,
+        EfiKeyIntl9
     }
     EFI_KEY;
 
